4ecf346decf85c84e1dde027f724eff2471bd418
Randy Weyer and Swati Sahay <randy@>
2018-06-13 11:41:52 -0700

    add business logic and user interface logic

---
+++ README.md
@@ -0,0 +1,36 @@
+# _Epicodus week4 day3_
+
+#### _Pig Dice Game, 06/13/2018_
+
+#### By _**Swati Sahay, Randy Weyer**_
+
+## Description
+
+## Specification
+* _Can be played by 2 people_
+* _Roll the dice and get the number_
+* _If the number is 1 remove the round and change the turn_
+* _Add number to the total if they hold the Dice_
+* _Show winner if you reached the goal number_
+
+
+## Setup/Installation Requirements
+
+* _Git clone this folder_
+* _Open the index.html in the browser_
+* _Fill in the blank and submit_
+* _Click on the full name to display full contact_
+
+## Support and contact details
+
+_Please contact RandyPWeyer@gmail.com  and swatiranjan0111@gmail.com for any issues, questions, ideas, or concerns._
+
+## Technologies Used
+
+_HTML, CSS, Bootstrap, JavaScript branching, arrays, and loops, jQuery functions, Github_
+
+### License
+
+*This software is licensed under the MIT license*
+
+Copyright (c) 2018 **_Randy Weyer and Swati Sahay_**
---
+++ js/scripts.js
@@ -0,0 +1,124 @@
+
+var total = 0;
+var roundScore = 0;
+var playerOneTurn = true;
+var playerTwoTurn = false;
+var playerOneScore = 0;
+var playerTwoScore = 0;
+
+var gameInput = function(players, dice, goal){
+  this.noOfPlayers = players;
+  this.noOfDice= dice;
+  this.goal=goal;
+}
+function roll(dice){
+  if (playerOneTurn){
+    // for (i=0;i<=dice;i++){
+      total = Math.floor((Math.random() * 5)+1);
+      console.log("Total: " + total);
+      if (total == 1){
+        total=0;
+        changeTurns();
+        // break;
+      }
+      roundScore=roundScore+total;
+      total=0;
+    // }
+    console.log(roundScore);
+
+  } else if (playerTwoTurn) {
+    // for (i=0;i<=dice;i++){
+      total = Math.floor((Math.random() * 5)+1);;
+      if (total == 1){
+        total=0;
+        changeTurns();
+        // break;
+      }
+    roundScore=roundScore+total;
+      total=0;
+    // }
+
+    roundScore+=total;
+
+  }
+}
+
+
+function changeTurns(){
+  checkWinner();
+  playerOneTurn = !playerOneTurn;
+  playerTwoTurn = !playerTwoTurn;
+  roundScore = 0;
+  $(".player-one").toggle();
+  $(".player-two").toggle();
+}
+function hold(){
+  if (playerOneTurn){
+    playerOneScore+=roundScore;
+  } else if (playerTwoTurn){
+    playerTwoScore+=roundScore;
+  }
+  roundScore = 0;
+  changeTurns();
+}
+function checkWinner(){
+if((playerOneScore>=50)){
+  alert("player 1 is winner")
+// $(".result").text("Winner is player 1")
+}
+else if (playerTwoScore>=50) {
+    alert("player 2 is winner")
+// $ (".result").text("Winner is player 2")
+}
+}
+
+
+
+
+$(document).ready(function(){
+  $("form#game-input").submit(function(event) {
+    event.preventDefault();
+    var playerNumber = $("#player-number").val();
+    var dice = $("#dice").val();
+    var goal = $("#goal").val();
+    var gameSetup = new gameInput(playerNumber, dice, goal);
+    $("#game-input").hide();
+
+    $(".initial-hide").show();
+
+    $(".player-two").hide();
+  });
+  $("#roll-player-one").click(function(event) {
+    event.preventDefault();
+    roll(dice);
+    $("#player-one-total").text(playerOneScore);
+    $("#player-one-round-total").text(roundScore);
+    $("#player-two-total").text(playerTwoScore);
+    $("#player-two-round-total").text(roundScore);
+  });
+  $("#hold-player-one").click(function(event) {
+    event.preventDefault();
+    hold();
+    $("#player-one-total").text(playerOneScore);
+    $("#player-one-round-total").text(roundScore);
+    $("#player-two-total").text(playerTwoScore);
+    $("#player-two-round-total").text(roundScore);
+  });
+  $("#roll-player-two").click(function(event) {
+    event.preventDefault();
+    roll(dice);
+    $("#player-one-total").text(playerOneScore);
+    $("#player-one-round-total").text(roundScore);
+    $("#player-two-total").text(playerTwoScore);
+    $("#player-two-round-total").text(roundScore);
+  });
+  $("#hold-player-two").click(function(event) {
+    event.preventDefault();
+    hold();
+    $("#player-one-total").text(playerOneScore);
+    $("#player-one-round-total").text(roundScore);
+    $("#player-two-total").text(playerTwoScore);
+    $("#player-two-round-total").text(roundScore);
+  });
+
+});
