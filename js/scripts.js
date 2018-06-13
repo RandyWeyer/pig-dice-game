98920d1945be2f01d5a245a41941f19719f3695c
Randy Weyer and Swati Sahay <swati@>
2018-06-13 15:18:35 -0700

    add computer logic for single player games

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@ var playerTwoTurn = false;
 var playerOneScore = 0;
 var playerTwoScore = 0;
 
+var gameSetup;
+
 var gameInput = function(players, dice, goal){
   this.noOfPlayers = players;
   this.noOfDice= dice;
@@ -14,28 +16,28 @@ var gameInput = function(players, dice, goal){
 function roll(dice){
   if (playerOneTurn){
     // for (i=0;i<=dice;i++){
-      total = Math.floor((Math.random() * 5)+1);
-      console.log("Total: " + total);
-      if (total == 1){
-        total=0;
-        changeTurns();
-        // break;
-      }
-      roundScore=roundScore+total;
+    total = Math.floor((Math.random() * 5)+1);
+    console.log("Total: " + total);
+    if (total == 1){
       total=0;
+      changeTurns();
+      // break;
+    }
+    roundScore=roundScore+total;
+    total=0;
     // }
-    console.log(roundScore);
+    // console.log(roundScore);
 
   } else if (playerTwoTurn) {
     // for (i=0;i<=dice;i++){
-      total = Math.floor((Math.random() * 5)+1);;
-      if (total == 1){
-        total=0;
-        changeTurns();
-        // break;
-      }
-    roundScore=roundScore+total;
+    total = Math.floor((Math.random() * 5)+1);;
+    if (total == 1){
       total=0;
+      changeTurns();
+      // break;
+    }
+    roundScore=roundScore+total;
+    total=0;
     // }
 
     roundScore+=total;
@@ -51,7 +53,26 @@ function changeTurns(){
   roundScore = 0;
   $(".player-one").toggle();
   $(".player-two").toggle();
-}
+
+  // Computer AI Logic
+  if (playerTwoTurn){
+  for(i=0; i<2 ;i++){
+    total = Math.floor((Math.random() * 5)+1);
+    console.log("Computer Roll: " + total);
+    if (total == 1){
+      total=0;
+      // changeTurns();
+      $("#player-two-total").text(playerTwoScore);
+      $("#player-two-round-total").text(roundScore);
+      break;
+    }
+    roundScore=roundScore+total;
+    total=0;
+  };
+  hold();
+  };
+};
+
 function hold(){
   if (playerOneTurn){
     playerOneScore+=roundScore;
@@ -93,16 +114,16 @@ $(document).ready(function(){
     roll(dice);
     $("#player-one-total").text(playerOneScore);
     $("#player-one-round-total").text(roundScore);
-    // $("#player-two-total").text(playerTwoScore);
-    // $("#player-two-round-total").text(roundScore);
+    $("#player-two-total").text(playerTwoScore);
+    $("#player-two-round-total").text(roundScore);
   });
   $("#hold-player-one").click(function(event) {
     event.preventDefault();
     hold();
     $("#player-one-total").text(playerOneScore);
     $("#player-one-round-total").text(roundScore);
-    // $("#player-two-total").text(playerTwoScore);
-    // $("#player-two-round-total").text(roundScore);
+    $("#player-two-total").text(playerTwoScore);
+    $("#player-two-round-total").text(roundScore);
   });
   $("#roll-player-two").click(function(event) {
     event.preventDefault();
